4799334bbb194e87ed8b9d6e46eee88488b4f02a
85891240@qq.com <85891240@qq.com>
2019-04-29 01:46:39 +0800

    add mybatis file

---
+++ _05mybatis/src/main/java/com/example/demo/DAO/AccountMapper.java
@@ -0,0 +1,27 @@
+package com.example.demo.DAO;
+
+import com.example.demo.Entity.Account;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.ManagedBean;
+import java.util.List;
+
+@Component
+@Mapper
+public interface AccountMapper {
+    @Insert("insert into account(name, money) values(#{name}, #{money})")
+    int add(@Param("name") String name, @Param("money") double money);
+
+    @Update("update account set name = #{name}, money = #{money} where id = #{id}")
+    int update(@Param("name") String name, @Param("money") double money, @Param("id") int  id);
+
+    @Delete("delete from account where id = #{id}")
+    int delete(int id);
+
+    @Select("select id, name as name, money as money from account where id = #{id}")
+    Account findAccount(@Param("id") int id);
+
+    @Select("select id, name as name, money as money from account")
+    List<Account> findAccountList();
+}
