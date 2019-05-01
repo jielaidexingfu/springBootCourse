b1cbb6e05075e49af5c7cbb5efa783a7f3aaa014
85891240@qq.com <85891240@qq.com>
2019-05-01 20:59:23 +0800

    add redis file

---
+++ _09redis/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import com.example.demo.dao.RedisDao;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+    public static Logger logger= LoggerFactory.getLogger(DemoApplicationTests.class);
+    @Test
+    public void contextLoads() {
+    }
+
+    @Autowired
+    RedisDao redisDao;
+    @Test
+    public void testRedis(){
+        redisDao.setKey("name","forezp");
+        redisDao.setKey("age","11");
+        logger.info(redisDao.getValue("name"));
+        logger.info(redisDao.getValue("age"));
+    }
+}
